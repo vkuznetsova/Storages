2aefbad5325dc537b222de8716a26a2d1e4a3163
Viktoria Kuznetsova <viktorya_pmik@mail.ru>
2014-06-10 17:57:46 +0400

    добавлен в представление comboBox, в котором записаны все id деревьев из БД

--- Storages/main.cpp
+++ Storages/main.cpp
@@ -9,23 +9,27 @@
 
 int main(int argc, char *argv[])
 {
-//    for(int i = 4; i < 4; i++)
-//    {
-//        QTime timeWrite;
-//        QTime timeRead;
-//        StorageTree tree = StorageTree::generateTree(i);
-//        tree.setID("idTree");
-//        QFile::remove("dataBaseName");
-//        StorageDatabaseWriter writer("dataBaseName");
-//        timeWrite.start();
-//        writer.write(tree);
-//        StorageDatabaseReader reader("dataBaseName");
-//        timeRead.start();
-//        const StorageTree actual = reader.read(tree.id());
-//        qDebug()<<"level = "<<i<<"time of write = "<<(double)timeWrite.elapsed()/1000
-//               <<"time of read = "
-//              <<(double)timeRead.elapsed()/1000;
-//    }
+    StorageDatabaseReader reader("dataBaseName");
+    reader.readID();
+
+    for(int i = 1; i < 16; i++)
+    {
+        QTime timeWrite;
+        QTime timeRead;
+        StorageTree tree = StorageTree::generateTree(i);
+        tree.setID("idTree" + QString::number(i));
+        QFile::remove("dataBaseName");
+        StorageDatabaseWriter writer("dataBaseName");
+        timeWrite.start();
+        writer.write(tree);
+        StorageDatabaseReader reader("dataBaseName");
+        timeRead.start();
+        const StorageTree actual = reader.read(tree.id());
+        qDebug()<<tree.id()<<"level = "<<i<<"time of write = "<<(double)timeWrite.elapsed()/1000
+               <<"time of read = "
+              <<(double)timeRead.elapsed()/1000;
+    }
+
 
 #ifdef TESTS
 
@@ -34,11 +38,11 @@ int main(int argc, char *argv[])
     QTest::qExec(&tStorageTree);
 
 
-        TTableModel tTableModel;
-        QTest::qExec(&tTableModel);
+    //        TTableModel tTableModel;
+    //        QTest::qExec(&tTableModel);
 
-    TStorageDatabaseInterface tStorageDatabaseInterface;
-    QTest::qExec(&tStorageDatabaseInterface);
+    //    TStorageDatabaseInterface tStorageDatabaseInterface;
+    //    QTest::qExec(&tStorageDatabaseInterface);
 
     return 0;
 #endif
