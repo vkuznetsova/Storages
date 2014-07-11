747867b8765b7592ea271b6fecde817d9f57ec0a
Viktoria Kuznetsova <viktorya_pmik@mail.ru>
2014-07-11 17:02:43 +0400

    Добавлен метод calcOrderPlan для расчета плана закупок для узлов.

--- Storages/OrderGenerator.cpp
+++ Storages/OrderGenerator.cpp
@@ -13,13 +13,35 @@ void OrderGenerator::calcOrderPlan(TreeOrderTable &orderTable,
     int expense = node.getExpence();
     int deliveryTime = node.getDeliveryTime();
     int currentBalance = balance;
-    for(int i = 0; i < days; i++)
+    Order order;
+    OrderPlan orderPlan;
+    for(int i = 1; i <= days; i++)
     {
         currentBalance -= expense;
+        if(currentBalance < 0)
+        {
+            currentBalance = 0;
+        }
         if(currentBalance == 0)
         {
-            OrderPlan().insertInc()
+            const QString from = node.getParent();
+            const QString to = node.id();
+            const int volumeOrder = deliveryTime * expense;
+            if((i - deliveryTime < 0))
+            {
+                continue;
+            }
+            order = Order(i, i - deliveryTime, volumeOrder, from, to);
+            orderPlan = OrderPlan().insertInc(i - deliveryTime, order);
+            orderTable.insertInc(storage, orderPlan);
         }
+        if(order.deliveryTime() == order.orderTime() + deliveryTime)
+        {
+            currentBalance += order.volumeOrder();
+        }
+        node.setBalance(currentBalance);
+        qDebug()<<orderTable.keys()<<orderPlan.keys()
+                << order.deliveryTime()<<order.orderTime()
+                <<order.volumeOrder()<<order.from()<<order.to();
     }
-
 }
--- Storages/TOrderGenerator.cpp
+++ Storages/TOrderGenerator.cpp
@@ -18,18 +18,83 @@ void TOrderGenerator::TestCalcOrderPlan_data()
                            << 0
                            << TreeOrderTable();
 
-    QTest::newRow("single")
+    QTest::newRow("single1")
+            << TreeOrderTable()
+            << (StorageTree().setRoot("s1")
+                .setBalance("s1", 0)
+                .setExpense("s1", 1)
+                .setDeliveryTime("s1", 0))
+            << "s1"
+            << 1
+            << (TreeOrderTable()
+                .insertInc("s1", OrderPlan()
+                           .insertInc(1, Order(1, 1, 0, QString(), "s1"))));
+
+    QTest::newRow("single2")
             << TreeOrderTable()
             << (StorageTree().setRoot("s1")
                 .setBalance("s1", 0)
                 .setExpense("s1", 1)
                 .setDeliveryTime("s1", 1))
             << "s1"
-            << 1
+            << 2
             << (TreeOrderTable()
                 .insertInc("s1", OrderPlan()
                            .insertInc(1, Order(2, 1, 1, QString(), "s1"))));
 
+    QTest::newRow("single3")
+            << TreeOrderTable()
+            << (StorageTree().setRoot("s1")
+                .setBalance("s1", 0)
+                .setExpense("s1", 1)
+                .setDeliveryTime("s1", 1))
+            << "s1"
+            << 3
+            << (TreeOrderTable()
+                .insertInc("s1", OrderPlan()
+                           .insertInc(1, Order(2, 1, 1, QString(), "s1")))
+                .insertInc("s1", OrderPlan()
+                           .insertInc(2, Order(3, 2, 1, QString(), "s1"))));
+
+    QTest::newRow("single4")
+            << TreeOrderTable()
+            << (StorageTree().setRoot("s1")
+                .setBalance("s1", 2)
+                .setExpense("s1", 1)
+                .setDeliveryTime("s1", 1))
+            << "s1"
+            << 3
+            << (TreeOrderTable()
+                .insertInc("s1", OrderPlan()
+                           .insertInc(1, Order(2, 1, 1, QString(), "s1")))
+                .insertInc("s1", OrderPlan()
+                           .insertInc(2, Order(3, 2, 1, QString(), "s1"))));
+
+    QTest::newRow("single5")
+            << TreeOrderTable()
+            << (StorageTree().setRoot("s1")
+                .setBalance("s1", 2)
+                .setExpense("s1", 3)
+                .setDeliveryTime("s1", 1))
+            << "s1"
+            << 3
+            << (TreeOrderTable()
+                .insertInc("s1", OrderPlan()
+                           .insertInc(1, Order(2, 1, 3, QString(), "s1")))
+                .insertInc("s1", OrderPlan()
+                           .insertInc(2, Order(3, 2, 3, QString(), "s1"))));
+
+    QTest::newRow("single6")
+            << TreeOrderTable()
+            << (StorageTree().setRoot("s1")
+                .setBalance("s1", 2)
+                .setExpense("s1", 3)
+                .setDeliveryTime("s1", 2))
+            << "s1"
+            << 3
+            << (TreeOrderTable()
+                .insertInc("s1", OrderPlan()
+                           .insertInc(1, Order(3, 1, 6, QString(), "s1"))));
 }
 
 void TOrderGenerator::TestCalcOrderPlan()
