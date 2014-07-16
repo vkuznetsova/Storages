5b5777c4df13b925a730dcbbfd70700fcc2fc68c
Viktoria Kuznetsova <viktorya_pmik@mail.ru>
2014-07-16 14:05:19 +0400

    переписаны тесты для расчета плана заказов в листьях дерева

--- Storages/OrderGenerator.cpp
+++ Storages/OrderGenerator.cpp
@@ -8,6 +8,10 @@ void OrderGenerator::calcOrderPlan(TreeOrderTable &orderTable,
                                    const StorageTree &tree,
                                    const QString &storage, const int days)
 {
+    if(storage.isNull())
+    {
+        return;
+    }
     StorageTreeNode node = tree.node(storage);
     int balance = node.getBalance();
     int expense = node.getExpence();
@@ -15,29 +19,68 @@ void OrderGenerator::calcOrderPlan(TreeOrderTable &orderTable,
     int currentBalance = balance;
     Order order;
     OrderPlan orderPlan;
+    QString from;
+    QString to;
+    int volumeOrder = 0;
     for(int i = 1; i <= days; i++)
     {
         currentBalance -= expense;
+        qDebug()<<i<<currentBalance;
         if(currentBalance < 0)
         {
             currentBalance = 0;
         }
-        if(currentBalance == 0)
+        if(currentBalance < expense || currentBalance == 0)
         {
-            const QString from = node.getParent();
-            const QString to = node.id();
-            const int volumeOrder = deliveryTime * expense;
-            if((i - deliveryTime) < 0)
+            QStringList childs = tree.childrenIDs(storage);
+            if(childs.size() == 0)
             {
-                continue;
+                from = node.getParent();
+                to = node.id();
+                qDebug()<< currentBalance;
+                volumeOrder = deliveryTime * expense - currentBalance;
+                currentBalance = volumeOrder;
+                if(i - deliveryTime <= 0)
+                {
+                    continue;
+                }
+                order = Order(i, i - deliveryTime, volumeOrder, from, to);
+                orderPlan = orderPlan.insertInc(i - deliveryTime, order);
             }
-            order = Order(i, i - deliveryTime, volumeOrder, from, to);
-            orderPlan = OrderPlan().insertInc(i - deliveryTime, order);
-            orderTable.insertInc(storage, orderPlan);
+            //            else
+            //            {
+            //                int expensesChilds = 0;
+            //                for(int j = 0; j < childs.size(); j++)
+            //                {
+            //                    from = node.getParent();
+            //                    to = node.id();
+            //                    expensesChilds += tree.node(childs[j]).getExpence();
+            //                }
+            //                volumeOrder = deliveryTime * expense + expensesChilds;
+            //                order = Order(i, i - deliveryTime, volumeOrder, from, to);
+            //                orderPlan = OrderPlan().insertInc(i - deliveryTime, order);
+            //            }
         }
         if(order.deliveryTime() == node.getDeliveryTime() + order.orderTime())
         {
-            node.setBalance(currentBalance + order.volumeOrder());
+            node.setBalance(order.volumeOrder());
         }
+    }
+    if(orderPlan.keys().size() == 0)
+    {
+        return;
+    }
+
+    orderTable.insertInc(storage, orderPlan);
+
+    qDebug()<< orderTable.keys();
+    foreach (const int it, orderPlan.keys())
+    {
+        qDebug()<< it
+                << order.deliveryTime()
+                << order.orderTime()
+                << order.volumeOrder()
+                << order.from()
+                << order.to();
     }
 }
